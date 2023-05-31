8a06df16660ebb249e02944012e50a1362409aea
hemanth0107 <hemanth.madala0107@gmail.com>
2023-05-31 12:05:03 +0530

    second commit

--- src/app/payement/page.js
+++ src/app/payement/page.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import fs from 'fs';
-import file from '/Users/M.Hemanth/my-project/aundefinedirbnb/public/payement.html'
+
 
 function page() {
    
   return (
-    <div dangerouslySetInnerHTML={{ __html: require('/Users/M.Hemanth/my-project/aundefinedirbnb/public/payement.html').default }} />
+    <div dangerouslySetInnerHTML={{ __html: require('../../../public/payement.html').default }} />
   )
 }
 
